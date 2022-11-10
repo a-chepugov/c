52303123f525fd4e9fdaf4da4a6ccb4c48d472c7
a.chepugov <a.chepugov@gmail.com>
2022-11-11 01:32:31 +0200

    chore

--- src/lib/guards.h
+++ src/lib/guards.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define GUARD(condition, label) if((!condition)) { goto label; }
-#define FUSE(condition, code, ...) if(!(condition)) { fprintf(stderr, "" __VA_ARGS__ ); exit(code); }
+#define FUSE(condition, code, ...) if(!(condition)) { fprintf(stderr, " " __VA_ARGS__ ); exit(code); }
