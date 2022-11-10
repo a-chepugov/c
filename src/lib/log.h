84f1d0881b7ca9ce266e44e8be0b95927befcc1c
a.chepugov <a.chepugov@gmail.com>
2022-11-11 01:34:13 +0200

    fix

--- src/lib/log.h
+++ src/lib/log.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <stdio.h>
+#include "helpers.h"
 
-#define LOG_TYPED(type, message, ...) fprintf(stderr, type " %s | %s:%d> " message "\n", __TIME__, __FILE__, __LINE__, "" __VA_ARGS__)
+#define LOG_TYPED(type, ...) fprintf(stderr, type " " __TIME__ " | "  __FILE__ ":" DEFER(STR, __LINE__ ) " > " __VA_ARGS__)
 #define LOG_INFO(...) LOG_TYPED("[INFO ]", __VA_ARGS__)
 #define LOG_WARN(...) LOG_TYPED("[WARN ]", __VA_ARGS__)
 #define LOG_ERR(...) LOG_TYPED("[ERROR]", __VA_ARGS__)
@@ -13,5 +14,5 @@
 #define DBG(...) LOG_TYPED("[-DBG-]", __VA_ARGS__)
 #endif
 
-#define ALERT(condition, ...) if(!(condition)) { fprintf(stderr, #condition "" __VA_ARGS__); }
+#define CHECK(condition, ...) if(!(condition)) { fprintf(stderr, #condition " " __VA_ARGS__); }
 
